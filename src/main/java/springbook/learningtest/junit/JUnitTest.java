7cbacfae38612a6a9e1e8c5b635ac5fdfaeb066e
양정수 <mycat83@gmail.com>
2019-08-22 18:01:59 +0900

    Fix 6-11

--- src/main/java/springbook/learningtest/junit/JUnitTest.java
+++ src/main/java/springbook/learningtest/junit/JUnitTest.java
@@ -13,6 +13,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.core.CombinableMatcher.either;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -31,32 +33,32 @@ public class JUnitTest {
     static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
     static ApplicationContext contextObject = null;
 
-    @Test
-    public void test1() {
-        assertThat(testObjects, not(hasItem(this)));
-        testObjects.add(this);
-
-        assertThat(contextObject == null || contextObject == this.context, is(true));
-        contextObject = this.context;
-    }
-
-    @Test
-    public void test2() {
-        assertThat(testObjects, not(hasItem(this)));
-        testObjects.add(this);
-
-        assertTrue(contextObject == null || contextObject == this.context);
-        contextObject = this.context;
-    }
-
-    @Test
-    public void test3() {
-        assertThat(testObjects, not(hasItem(this)));
-        testObjects.add(this);
-
-        assertThat(contextObject, either(is(nullValue())).or(CoreMatchers.<Object>is(this.context)));
-        contextObject = this.context;
-    }
+//    @Test
+//    public void test1() {
+//        assertThat(testObjects, not(hasItem(this)));
+//        testObjects.add(this);
+//
+//        assertThat(contextObject == null || contextObject == this.context, is(true));
+//        contextObject = this.context;
+//    }
+//
+//    @Test
+//    public void test2() {
+//        assertThat(testObjects, not(hasItem(this)));
+//        testObjects.add(this);
+//
+//        assertTrue(contextObject == null || contextObject == this.context);
+//        contextObject = this.context;
+//    }
+//
+//    @Test
+//    public void test3() {
+//        assertThat(testObjects, not(hasItem(this)));
+//        testObjects.add(this);
+//
+//        assertThat(contextObject, either(is(nullValue())).or(CoreMatchers.<Object>is(this.context)));
+//        contextObject = this.context;
+//    }
 
     @Test
     public void singletonBean() {
