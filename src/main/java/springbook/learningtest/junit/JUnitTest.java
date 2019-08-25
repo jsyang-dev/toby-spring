41c40e5428ca5714281319efc924974edc40c19a
양정수 <mycat83@gmail.com>
2019-08-25 22:27:11 +0900

    Fix 7-1

--- src/main/java/springbook/learningtest/junit/JUnitTest.java
+++ src/main/java/springbook/learningtest/junit/JUnitTest.java
@@ -1,6 +1,5 @@
 package springbook.learningtest.junit;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +11,10 @@ import springbook.user.dao.UserDaoJdbc;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.core.CombinableMatcher.either;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/junit.xml")
@@ -33,15 +31,15 @@ public class JUnitTest {
     static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
     static ApplicationContext contextObject = null;
 
-//    @Test
-//    public void test1() {
-//        assertThat(testObjects, not(hasItem(this)));
-//        testObjects.add(this);
-//
-//        assertThat(contextObject == null || contextObject == this.context, is(true));
-//        contextObject = this.context;
-//    }
-//
+    @Test
+    public void test1() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
+    }
+
 //    @Test
 //    public void test2() {
 //        assertThat(testObjects, not(hasItem(this)));
@@ -49,14 +47,14 @@ public class JUnitTest {
 //
 //        assertTrue(contextObject == null || contextObject == this.context);
 //        contextObject = this.context;
-//    }
-//
+//    }context
+
 //    @Test
 //    public void test3() {
 //        assertThat(testObjects, not(hasItem(this)));
 //        testObjects.add(this);
 //
-//        assertThat(contextObject, either(is(nullValue())).or(CoreMatchers.<Object>is(this.context)));
+//        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
 //        contextObject = this.context;
 //    }
 
--- src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -4,6 +4,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
+import springbook.user.sqlservice.SqlService;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
@@ -12,6 +13,11 @@ import java.util.List;
 
 public class UserDaoJdbc implements UserDao {
     private JdbcTemplate jdbcTemplate;
+    private SqlService sqlService;
+
+    public void setSqlService(SqlService sqlService) {
+        this.sqlService = sqlService;
+    }
 
     private RowMapper<User> userMapper =
             new RowMapper<User>() {
@@ -33,28 +39,28 @@ public class UserDaoJdbc implements UserDao {
     }
 
     public void add(final User user) {
-        this.jdbcTemplate.update("insert into users (id, name, password, level, login, recommend, email) values (?, ?, ?, ?, ?, ?, ?)",
+        this.jdbcTemplate.update(this.sqlService.getSql("userAdd"),
                 user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail());
     }
 
     public User get(String id) {
-        return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[]{id}, userMapper);
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGet"), new Object[]{id}, userMapper);
     }
 
     public void deleteAll() {
-        this.jdbcTemplate.update("delete from users");
+        this.jdbcTemplate.update(this.sqlService.getSql("userDeleteAll"));
     }
 
     public Integer getCount() {
-        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+        return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGetCount"), Integer.class);
     }
 
     public List<User> getAll() {
-        return this.jdbcTemplate.query("select * from users order by id", userMapper);
+        return this.jdbcTemplate.query(this.sqlService.getSql("userGetAll"), userMapper);
     }
 
     public void update(User user) {
-        this.jdbcTemplate.update("update users set name = ?, password = ?, level = ?, login = ?, recommend = ?, email = ? where id = ?",
+        this.jdbcTemplate.update(this.sqlService.getSql("userUpdate"),
                 user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
     }
 }
