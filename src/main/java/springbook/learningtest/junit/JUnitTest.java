515bb189c746b843a7b4dedfaf60bea1e09e557f
양정수 <mycat83@gmail.com>
2019-08-26 17:58:30 +0900

    Fix 7-2

--- src/main/java/springbook/learningtest/junit/JUnitTest.java
+++ src/main/java/springbook/learningtest/junit/JUnitTest.java
@@ -1,5 +1,6 @@
 package springbook.learningtest.junit;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,11 @@ import springbook.user.dao.UserDaoJdbc;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.matchers.JUnitMatchers.either;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/junit.xml")
@@ -47,14 +49,14 @@ public class JUnitTest {
 //
 //        assertTrue(contextObject == null || contextObject == this.context);
 //        contextObject = this.context;
-//    }context
-
+//    }
+//
 //    @Test
 //    public void test3() {
 //        assertThat(testObjects, not(hasItem(this)));
 //        testObjects.add(this);
 //
-//        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+//        assertThat(contextObject, either(is(nullValue())).or(CoreMatchers.<Object>is(this.context)));
 //        contextObject = this.context;
 //    }
 
