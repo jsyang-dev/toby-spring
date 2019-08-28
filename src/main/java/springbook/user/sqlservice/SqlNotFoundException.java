1b3a21fd47b2d99d0bd9fdd9783717a76d469592
양정수 <mycat83@gmail.com>
2019-08-28 22:32:03 +0900

    Fix 7-7

--- src/main/java/springbook/user/sqlservice/SqlNotFoundException.java
+++ src/main/java/springbook/user/sqlservice/SqlNotFoundException.java
@@ -1,4 +1,19 @@
 package springbook.user.sqlservice;
 
 public class SqlNotFoundException extends RuntimeException {
+    public SqlNotFoundException() {
+        super();
+    }
+
+    public SqlNotFoundException(String message) {
+        super(message);
+    }
+
+    public SqlNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public SqlNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
