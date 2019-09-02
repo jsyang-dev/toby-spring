c92bf4db29a04ef39d7b308628fbd22301e23e73
yangjs <mycat83@gmail.com>
2019-09-02 22:05:36 +0900

    Fix 7-13

--- src/main/java/springbook/EnableSqlService.java
+++ src/main/java/springbook/EnableSqlService.java
@@ -2,6 +2,9 @@ package springbook;
 
 import org.springframework.context.annotation.Import;
 
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
 @Import(SqlServiceContext.class)
 public @interface EnableSqlService {
 }
